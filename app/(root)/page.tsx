f203ded0711558ba40a8f322adca9ee78c5fe819
Mohamed Hema <mohamedhema2017@gmail.com>
2024-01-24 16:41:12 +0200

    adding assits

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,8 +4,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
+      <h1 className="text-4xl">Evently</h1>
       <Button variant="destructive" className="px-10">
-        Hello
+        Delete
       </Button>
     </main>
   );
