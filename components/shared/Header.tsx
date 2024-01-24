7f207f4802f03be2c014909387ee81f63a72650c
Mohamed Hema <mohamedhema2017@gmail.com>
2024-01-24 18:20:32 +0200

    clerk implementation is done

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "../ui/button";
 import NavItems from "./NavItems";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
@@ -16,11 +17,16 @@ const Header = () => {
             alt="evently"
           />
         </Link>
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
 
         <div className="flex w-32 justify-end gap-3">
           <SignedIn>
             <UserButton afterSignOutUrl="/" />
-            <NavItems />
+            <MobileNav />
           </SignedIn>
           <SignedOut>
             <Button asChild className="rounded-full" size="lg">
